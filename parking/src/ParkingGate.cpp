0dae60de0d19af2e9adb7b0af0ffeb4242661d35
2089061-MJ <2089061@pcu.ac.kr>
2025-10-29 19:08:41 +0900

    ParkingGate.cpp 수정(초음파 기능 제거)

--- parking/src/ParkingGate.cpp
+++ parking/src/ParkingGate.cpp
@@ -1,14 +1,12 @@
 #include "ParkingGate.h"
 
-ParkingGate::ParkingGate(uint8_t servoPin, uint8_t trigPin, uint8_t echoPin)
-    : SERVO_PIN(servoPin), TRIG_PIN(trigPin), ECHO_PIN(echoPin), state(0) {
+ParkingGate::ParkingGate(uint8_t servoPin)
+    : SERVO_PIN(servoPin), state(0) {
 }
 
 void ParkingGate::begin() {
     gateServo.attach(SERVO_PIN);
     gateServo.write(0);  // 초기 닫힘
-    pinMode(TRIG_PIN, OUTPUT);
-    pinMode(ECHO_PIN, INPUT);
     command = "";
 }
 
@@ -25,34 +23,18 @@ void ParkingGate::update() {
                 if (command == "open") {
                     gateServo.write(120);
                     Serial.println("게이트 열림");
+                    state = 1;
                 }
                 else if (command == "close") {
                     gateServo.write(0);
                     Serial.println("게이트 닫힘");
+                    state = 0;
                 }
             }
             command = "";
         } else {
             command += c;
         }
-    }
-
-    // 초음파 거리 측정
-    float distance = measureDistance();
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" cm");
-
-    // 자동 열림/닫힘 로직
-    if (distance < 8 && state == 0) {
-        Serial.println("가까이 접근 - 게이트 열림");
-        openGate();
-        delay(3000);
-    }
-    else if (distance >= 9 && state == 1) {
-        Serial.println("멀어짐 - 게이트 닫힘");
-        closeGate();
-        delay(3000);
     }
 
     delay(500);
@@ -66,15 +48,4 @@ void ParkingGate::openGate() {
 void ParkingGate::closeGate() {
     gateServo.write(0);
     state = 0;
-}
-
-float ParkingGate::measureDistance() {
-    digitalWrite(TRIG_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-
-    unsigned long duration = pulseIn(ECHO_PIN, HIGH);
-    return duration * 0.034 / 2;
 }
